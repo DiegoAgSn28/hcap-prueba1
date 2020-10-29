f806c163cf726fafccf15f13c903da0a7756e100
DiegoAgSn28 <a01745112@itesm.mx>
2020-10-29 13:33:34 -0600

    matriz

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2
 
 imagen= cv2.imread("imagen.jpg")
@@ -15,9 +16,15 @@ imagen= cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 print(imagen.shape)
 print(imagen[0][0])
 
-imagen[0][0]=0;
-imagen[0][1]=0;
-imagen[0][2]=0;
+imagen[0][0]=0
+imagen[0][1]=0
+imagen[0][2]=0
 
 cv2.imwrite("grayimage.jpg", imagen)
+
+matriz=np.zeros((256,256),np.float32)
+imagen= cv2.cvtColor(matriz, cv2.COLOR_GRAY2BGR)
+print(imagen.shape)
+cv2.imwrite("matrizcolorimagen.jpg", imagen)
+
 

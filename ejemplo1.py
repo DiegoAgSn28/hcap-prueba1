5e788f1293e6a7b182673a55c47de8217b985251
DiegoAgSn28 <a01745112@itesm.mx>
2020-10-29 13:03:39 -0600

    Nueva imagen

--- ejemplo1.py
+++ ejemplo1.py
@@ -3,8 +3,17 @@ import cv2
 imagen= cv2.imread("imagen.jpg")
 imagen= cv2.cvtColor(imagen,cv2.COLOR_BGR2RGB)
 print(imagen.shape)
-print(imagen[0][0][0])
+print(imagen[0][0])
 imagen= cv2.resize(imagen, (256,256))
 
 cv2.imwrite("resizeimagen.jpg", imagen)
-cv2.imshow("image", imagen)
+#cv2.imshow("image", imagen)
+
+imagen= cv2.imread("imagen.jpg")
+imagen= cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+print(imagen.shape)
+print(imagen[0][0])
+
+cv2.imwrite("grayimage.jpg", imagen)
+
